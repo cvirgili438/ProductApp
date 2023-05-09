4f81801b1be0203edb66a6f40a5ab6c113d5a3f8
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2023-05-09 16:34:38 -0300

    first commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using CarlosAPP.Data;
+using CarlosAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarlosAPP.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //GET
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _db.Products;
+            return View(products);
+        }
+        //GET
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid) 
+            {
+                TempData["error"] = "Product didn't create";
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(product);
+                _db.SaveChanges();
+                TempData["success"] = "Product was Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+        //GET
+        public IActionResult Edit(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Find(id);
+            if (product == null) { return NotFound(); }
+            return View(product);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["error"] = "Product didn't update";
+            }
+            if (ModelState.IsValid)             
+            {
+                _db.Products.Update(product);
+                _db.SaveChanges();
+                TempData["success"] = "Product was Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarlosAPP.Models
+{
+    public class Product
+    {
+       
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Nombre")]
+        public string? Name { get; set; }
+        [Required]
+        [DisplayName("Descripcion")]
+        public string? Description { get; set; }
+        [Required]
+        [DisplayName("Precio en USD")]
+        public decimal PriceUSD { get; set; }
+        [Required]
+        [DisplayName("Imagen")]
+        [Url]
+        public string? ImageURL { get; set; }
+
+
+    }
+
+}
